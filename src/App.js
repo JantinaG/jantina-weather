85dba682708001ccebaa1d71c5b4df277d45904f
Jantina Eefting <65661797+JantinaG@users.noreply.github.com>
2020-08-16 19:01:28 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-     <p>Weather App</p>
+     <h1>Weather App</h1>
+      <footer>Coded by Jantina Eefting and is {" "} <a href="https://github.com/JantinaG/jantina-weather" target="blank" > open sourced</a> </footer>
     </div>
   );
 }
